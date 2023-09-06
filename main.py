50bc12349f5fa33089eee3d30c1ef62b0701521c
ibram5 <143288555+ibram5@users.noreply.github.com>
2023-09-06 19:07:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+person = {
+    "name": "ibram",
+    "age" : 17,
+    "hobbies":["football", "reading ", "gaming"]
+    
+}
+
+print(person['name'])
+print(person['age'])
+ 
+ 
+person["age"] = 18
+person["country"] = "Egypt"
+print(person)
+print(person.values())
+
+person['hobbies'].append("basketball")
+
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW YOU ARE AMAZING")
+        
+check_hobbies(person)
+        
